06f977d86a9b2aa7d325dd911a226e5f67a5f6b9
Kirit Sælensminde <k@kirit.com>
2011-06-30 11:21:19 +0700

    Switched on the Django nose test runner.

--- test-projects/django_latest/settings.py
+++ test-projects/django_latest/settings.py
@@ -124,6 +124,9 @@ INSTALLED_APPS = (
     'async_exec'
 )
 
+# Needed to get the Django nose test runner working
+TEST_RUNNER='django_nose.NoseTestSuiteRunner'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
