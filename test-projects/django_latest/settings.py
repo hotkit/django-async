3c8dbbd5bbda85a625c26ddd84c36f6bea663026
Kirit Sælensminde <k@kirit.com>
2011-06-30 11:01:02 +0700

    Added the async exec module.

--- test-projects/django_latest/settings.py
+++ test-projects/django_latest/settings.py
@@ -119,6 +119,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'async_exec'
 )
 
 # A sample logging configuration. The only tangible logging
