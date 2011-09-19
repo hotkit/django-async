5a5c3cef35fa9fed0bf952a59b90b8ebefbf7f2b
juacompe <juacompe@gmail.com>
2011-09-20 02:29:57 +0700

    removes irrelevant docstring

--- async_exec/models.py
+++ async_exec/models.py
@@ -1,24 +1,12 @@
 from django.db.models import Model, CharField, TextField, DateTimeField
 
 class Job(Model):
-    """Execute a Python function asynchronously.
-
-    Do not create these objects directly.
-    Use the function create to make them.
-    """
     name = CharField(max_length = 100, blank=False)
     args = TextField()
     kwargs = TextField()
-
-    scheduled = DateTimeField(
-        help_text = "If not set, will be executed ASAP",
-        null = True,
-        blank = True
-    )
-    executed = DateTimeField(
-        null = True,
-        blank = True
-    )
+    scheduled = DateTimeField(null = True, blank = True, 
+        help_text = "If not set, will be executed ASAP")
+    executed = DateTimeField(null = True, blank = True)
 
     def __unicode__(self):
         return self.name
