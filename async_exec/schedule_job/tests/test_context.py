c94a711ebdd756d907146ce8bb131e464c7ae04d
juacompe <juacompe@gmail.com>
2011-09-20 02:08:00 +0700

    - refactor job persisting check out as another test as it is a success post condition - refactor job initialization into setUp

--- async_exec/schedule_job/tests/test_context.py
+++ async_exec/schedule_job/tests/test_context.py
@@ -5,23 +5,24 @@ from async_exec.schedule_job.tests.function_for_test import sample_function
 from async_exec.models import Job
 
 class TestScheduleJob(TestCase):
+    def setUp(self):
+        self.job = Job()
+
+    def test_job_is_persisted(self):
+        with ScheduleJob(None, self.job, sample_function) as context:
+            self.assertTrue(context.job.id)
+        
     def test_successfully_schedule_job_with_no_scheduled_time(self):
-        job = Job()
-        with ScheduleJob(None, job, sample_function) as schedule_job:
-            # Conditions may not change in here because post conditions are met! 
-            self.assertTrue(schedule_job.job.id)
-            self.assertEqual(None, schedule_job.job.scheduled)
+        with ScheduleJob(None, self.job, sample_function) as context:
+            self.assertEqual(None, context.job.scheduled)
 
     def test_successfully_schedule_job_with_scheduled_time(self):
-        job = Job()
         scheduled_time = datetime.now()
-        with ScheduleJob(scheduled_time, job, sample_function) as schedule_job:
-            # Conditions may not change in here because post conditions are met! 
-            self.assertEqual(scheduled_time, schedule_job.job.scheduled)
+        with ScheduleJob(scheduled_time, self.job, sample_function) as context:
+            self.assertEqual(scheduled_time, context.job.scheduled)
         
     def test_successfully_schedule_job_with_kwargs(self):
-        job = Job()
-        with ScheduleJob(None, job, sample_function, name = 'John') as schedule_job:
+        with ScheduleJob(None, self.job, sample_function, name = 'John') as context:
             # Conditions may not change in here because post conditions are met! 
-            self.assertEqual('{"name": "John"}', schedule_job.job.kwargs)
+            self.assertEqual('{"name": "John"}', context.job.kwargs)
         
