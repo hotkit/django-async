5c75407582f4a2102f8edcadbaa275bef998520d
juacompe <juacompe@gmail.com>
2011-09-21 02:41:41 +0700

    - try to load 'async_exec.tests.function_for_test' as module but it didn't work - about to try reduce

--- async_exec/execute_job/role.py
+++ async_exec/execute_job/role.py
@@ -1,6 +1,23 @@
 from datetime import datetime
 from roles.django import ModelRoleType
 
+def load_function(full_name):
+    module_name, function_name = split(full_name)
+    module = __import__(module_name)
+    function = getattr(module, function_name)
+    return function 
+
+def split(full_name):
+    """
+    Parameter:
+    full_name = fully qualified name of a function
+    Return (module_name, function_name)
+    """
+    items = full_name.split('.')
+    function_name = items[-1]
+    module_name = '.'.join(items[0:-1])
+    return module_name, function_name
+
 class Process(object):
     __metaclass__ = ModelRoleType
 
--- async_exec/execute_job/tests/test_role.py
+++ async_exec/execute_job/tests/test_role.py
@@ -1,21 +1,40 @@
-from async_exec.execute_job.role import Process
+from async_exec.execute_job.role import Process, load_function, split
+from async_exec.tests.function_for_test import hello_world
 from datetime import datetime
 from django.test import TestCase
 from yas.stub import ModelStub
 
 class TestProcess(TestCase):
+    def setUp(self):
+        self.process = Process(ModelStub())
+        
     def test_is_execute_is_true_if_executed_time_is_set(self):
-        process = Process(ModelStub())
-        process.executed = datetime.now()
-        self.assertTrue(process.is_executed())
+        self.process.executed = datetime.now()
+        self.assertTrue(self.process.is_executed())
         
     def test_is_execute_is_false_if_executed_time_is_not_set(self):
-        process = Process(ModelStub())
-        process.executed = None
-        self.assertFalse(process.is_executed())
+        self.process.executed = None
+        self.assertFalse(self.process.is_executed())
 
     def test_execute_set_execution_time(self):
-        process = Process(ModelStub())
-        process.execute() 
-        self.assertIsNotNone(process.executed)
+        self.process.execute() 
+        self.assertIsNotNone(self.process.executed)
+
+    def test_execute_really_call_function(self):
+        pass
+
+
+class TestLoadFunction(TestCase):
+    def test_load_function(self):
+        function_name = 'async_exec.tests.function_for_test.hello_world'
+        function = load_function(function_name)
+        self.assertEqual(hello_world, function)
+
+
+class TestSplit(TestCase):
+    def test_split(self):
+        full_name = 'async_exec.tests.function_for_test.hello_world'
+        module_name, function_name = split(full_name)
+        self.assertEqual('async_exec.tests.function_for_test', module_name)
+        self.assertEqual('hello_world', function_name)
 
