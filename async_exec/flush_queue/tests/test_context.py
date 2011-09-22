3c2f7f07cd15fc3d075010c82aa1996edf58b7d3
juacompe <juacompe@gmail.com>
2011-09-23 01:49:21 +0700

    - add tests for jobs scheduled in future, all pass

--- async_exec/flush_queue/tests/test_context.py
+++ async_exec/flush_queue/tests/test_context.py
@@ -10,7 +10,7 @@ class TestFlushEmptyQueue(TestCase):
         with FlushQueue(queue) as context:
             self.assertIsNone(context.queue.next_job())
 
-class TestFlushQueueWithSchedukedJobs(TestCase):
+class TestFlushQueueWithJobsScheduledInPast(TestCase):
     def setUp(self):
         yesterday = datetime.now() - timedelta(days=1)
         function_name = 'async_exec.tests.function_for_test.hello_world'
@@ -18,11 +18,36 @@ class TestFlushQueueWithSchedukedJobs(TestCase):
         Job(name = function_name, scheduled = yesterday).save()
         Job(name = function_name, scheduled = yesterday).save()
 
-    def test_no_next_job_left_after_flush_queue__past_jobs_in_queue(self):
+    def test_no_next_job_left_after_flushed_queue(self):
         queue = Job.objects
         with FlushQueue(queue) as context:
             self.assertIsNone(context.queue.next_job())
+
+    def test_all_job_are_executed_after_flushed_queue(self):
+        queue = Job.objects
+        with FlushQueue(queue) as context:
+            pass
         number_of_executed_jobs = Job.objects.filter(executed__isnull = False).count()
         self.assertEqual(3, number_of_executed_jobs)
         
+
+class TestFlushQueueWithJobsScheduledInFuture(TestCase):
+    def setUp(self):
+        tomorrow = datetime.now() + timedelta(days=1)
+        function_name = 'async_exec.tests.function_for_test.hello_world'
+        Job(name = function_name, scheduled = tomorrow).save()
+        Job(name = function_name, scheduled = tomorrow).save()
+        Job(name = function_name, scheduled = tomorrow).save()
+
+    def test_no_job_is_executed_after_flushed_queue(self):
+        queue = Job.objects
+        with FlushQueue(queue) as context:
+            pass
+        number_of_executed_jobs = Job.objects.filter(executed__isnull = False).count()
+        self.assertEqual(0, number_of_executed_jobs)
+
+    def test_no_next_job_after_flushed_queue(self):
+        queue = Job.objects
+        with FlushQueue(queue) as context:
+            self.assertIsNone(context.queue.next_job())
 
