f335b2aaf8bfd3abf0fd222058ee271b6a4cc06d
juacompe <juacompe@lufia.(none)>
2011-10-30 15:50:34 +0700

    add test in schedule job in case a scheduling function has parameter named kwargs

--- async_exec/schedule_job/tests/test_context.py
+++ async_exec/schedule_job/tests/test_context.py
@@ -1,5 +1,5 @@
 from async_exec.schedule_job.context import ScheduleJob
-from async_exec.tests.function_for_test import sample_function
+from async_exec.tests.function_for_test import sample_function, function_that_takes_kwargs
 from async_exec.models import Job
 from datetime import datetime
 from django.test import TestCase
@@ -22,6 +22,27 @@ class TestScheduleJob(TestCase):
             self.assertEqual(scheduled_time, context.job.scheduled)
         
     def test_successfully_schedule_job_with_kwargs(self):
-        with ScheduleJob(None, self.job, sample_function, name = 'John') as context:
+        """
+        Scenario:
+            trying the schedule the line below
+            sample_function(name = 'John')
+        Expected:
+            the kwargs are persisted
+        """
+        with ScheduleJob(None, self.job, 
+                 sample_function, name = 'John') as context:
             self.assertEqual('{"name": "John"}', context.job.kwargs)
         
+    def test_successfully_schedule_job_parameter_named_kwargs(self):
+        """
+        Scenario:
+            trying the schedule the line below
+            function_that_takes_kwargs(kwargs = 'abc')
+        Expected:
+            the kwargs are persisted
+        """
+        with ScheduleJob(None, self.job, 
+                 function_that_takes_kwargs, 
+                 kwargs = 'abc') as context:
+            self.assertEqual('{"kwargs": "abc"}', context.job.kwargs)
+
