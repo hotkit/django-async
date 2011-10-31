218b969630b8e6be83f3e704b324bbdcb9b5ca04
juacompe <juacompe@lufia.(none)>
2011-10-31 17:44:45 +0700

    - refactor duplicated code out - use played_by function for cleaner role assignment

--- async_exec/execute_jobs/context.py
+++ async_exec/execute_jobs/context.py
@@ -7,25 +7,21 @@ class ExecuteJobs(object):
         self.economy_queue = EconomyQueue(queue, method = clone)
 
     def __enter__(self):
-        while True:
-            while True:
-                next_job = self.priority_queue.find_first_job()
-                if not next_job:
-                    break
-                Process(next_job)
-                next_job.execute()
-                next_job.save()
-                Process.revoke(next_job) 
-            next_job = self.economy_queue.find_first_job()
-            if not next_job:
-                break
-            Process(next_job)
-            next_job.execute()
-            next_job.save()
-            Process.revoke(next_job) 
+        while process(self.priority_queue):
+            pass
+        while process(self.economy_queue): 
+            pass
         return self
 
     def __exit__(self, type, value, traceback):
         PriorityQueue.revoke(self.priority_queue)
         EconomyQueue.revoke(self.economy_queue)
 
+def process(queue):
+    next_job = queue.find_first_job()
+    if not next_job:
+        return False
+    with Process.played_by(next_job):
+        next_job.execute()
+        next_job.save()
+    return True
