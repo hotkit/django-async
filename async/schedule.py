097c6f2a988111b2d084db7e84aeec7f7e8e19be
Kirit Sælensminde <k@kirit.com>
2012-05-19 12:56:47 +0700

    Move the import so it doesn't interfere with coverage.

--- async/schedule.py
+++ async/schedule.py
@@ -1,11 +1,11 @@
 """
     Schedule the execution of an async task.
 """
-from async.models import Job
 
 
 def schedule(function, args = None, kwargs = None,
         run_after= None, meta = None):
     """Schedule a tast for execution.
     """
+    from async.models import Job # Don't want to import from __init__
     return Job.objects.create()
