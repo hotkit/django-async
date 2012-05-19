2f550bff01cb3f37338f2b0befbdcb56423e027d
Kirit Sælensminde <k@kirit.com>
2012-05-19 12:37:07 +0700

    Fixed up the setup file so it can be installed more easily.

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 import os
 from setuptools import setup
 
-def read(fname):
+def read(fname1, fname2):
+    if os.path.exists(fname1):
+        fname = fname1
+    else:
+        fname = fname2
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "django_async",
-    version = "0.2.2",
+    name = "django-async",
+    version = "0.2.2.1",
     author_email = "kirit@felspar.com",
     description = ("Asynchronous task execution for Django"),
-    long_description = read('README.markdown'),
+    long_description = read('README','README.markdown'),
     license = "Boost Software License - Version 1.0 - August 17th, 2003",
     keywords = "django async taskqueue task queue",
     classifiers = [
@@ -19,12 +23,9 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: Boost Software License - Version 1.0 - August 17th, 2003",
     ],
-    packages = ['async_exec',
-        'async_exec.execute_jobs',
-        'async_exec.management',
-        'async_exec.management.commands',
-        'async_exec.schedule_job'],
-    install_requires = ['roles'],
-    dependency_links = [
-        'git://github.com/amolenaar/roles.git'],
+    install_requires = ['simplejson'],
+    packages = ['async',
+        'async.tests',
+        'async.management',
+        'async.management.commands'],
 )
