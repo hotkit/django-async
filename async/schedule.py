90f86096455d07c608bcdf0680bb017da10cee0f
Kirit Sælensminde <k@kirit.com>
2012-05-19 13:01:13 +0700

    Initial implementation of the scheduler.

--- async/schedule.py
+++ async/schedule.py
@@ -1,6 +1,7 @@
 """
     Schedule the execution of an async task.
 """
+from simplejson import dumps
 
 
 def schedule(function, args = None, kwargs = None,
@@ -8,4 +9,8 @@ def schedule(function, args = None, kwargs = None,
     """Schedule a tast for execution.
     """
     from async.models import Job # Don't want to import from __init__
-    return Job.objects.create()
+    job = Job(
+        name=function, args=dumps(args or []), kwargs=dumps(kwargs or {}),
+        meta=dumps(meta or {}), retried=0, notes='', scheduled=run_after)
+    job.save()
+    return job
