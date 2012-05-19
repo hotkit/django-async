1dea9cd8a5d60427e9ae441053623fea3d9e6c8f
Kirit Sælensminde <k@kirit.com>
2012-05-19 12:42:23 +0700

    Added skeleton implementation.

---
+++ async/management/commands/flush_queue.py
@@ -0,0 +1,22 @@
+"""
+    Django Async management commands.
+"""
+from optparse import make_option
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+        Invoke using:
+            python manage.py flush_queue
+    """
+    option_list = BaseCommand.option_list + (
+        make_option('--jobs', '-j', dest='jobs',
+            help='The maximum number of jobs to run'),
+    )
+    help = 'Does a single pass over the asynchronous queue'
+
+    def handle(self, **options):
+        """Command implementation.
+        """
+        pass
