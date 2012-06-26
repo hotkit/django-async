6fd6fa845eff0e78f58d17d4e445d643ed6452fb
Kirit Sælensminde <k@kirit.com>
2012-06-26 19:06:42 +0700

    Added a basic implementation of the operation.

--- async/tests/test_slumber_server.py
+++ async/tests/test_slumber_server.py
@@ -1,8 +1,16 @@
 """
     Tests for the Slumber operations.
 """
+from django.contrib.auth.models import User, Permission
 from django.test import TestCase
 from simplejson import loads
+
+from async.models import Job
+
+
+# Instance of 'WSGIRequest' has no 'status_code' member
+#  (but some types could not be inferred)
+# pylint: disable=E1103
 
 
 class TestSlumber(TestCase):
@@ -13,9 +21,6 @@ class TestSlumber(TestCase):
     def test_slumber_root(self):
         """Make sure Slumber is properly wired in.
         """
-        # Instance of 'WSGIRequest' has no 'status_code' member
-        #  (but some types could not be inferred)
-        # pylint: disable=E1103
 
         response = self.client.get('/slumber/')
         self.assertEqual(response.status_code, 200)
@@ -35,4 +40,39 @@ class TestSlumber(TestCase):
                     '/slumber/django/contrib/staticfiles/',
                 'django_nose': '/slumber/django_nose/'},
             _meta={'message': 'OK', 'status': 200}))
+
+
+class TestSchedule(TestCase):
+    """Make sure the schedule API wrapper works.
+    """
+    URL = '/slumber/async/Job/schedule/'
+    def setUp(self):
+        super(TestSchedule, self).setUp()
+        self.user = User.objects.create(username='test')
+        self.user.set_password('password')
+        self.user.save()
+        self.client.login(username='test', password='password')
+        self.permission = Permission.objects.get(codename='add_job')
+        self.user.user_permissions.add(self.permission)
+
+    def test_get_works(self):
+        """Make sure the operation is wired in. Don't expect any output yet.
+        """
+        response = self.client.get(self.URL)
+        self.assertEqual(response.status_code, 200)
+        json = loads(response.content)
+        self.assertEqual(json, dict(
+            _meta={'message': 'OK', 'status': 200, 'username': 'test'}))
+
+    def test_simple_post_works(self):
+        """Make sure the basic post functionality works.
+        """
+        response = self.client.post(self.URL, dict(
+            name='test-job-1'))
+        self.assertEqual(response.status_code, 200)
+        job = Job.objects.get(name='test-job-1')
+        json = loads(response.content)
+        self.assertEqual(json, dict(
+            job=dict(id=job.id),
+            _meta={'message': 'OK', 'status': 200, 'username': 'test'}))
 
