6a18b4ca0f8f9a08348a6fd2df91cf6ddeb9414a
Kirit Sælensminde <k@kirit.com>
2013-09-10 12:18:17 +0100

    Make the lock acquisition a decorator

--- async/management/commands/flush_queue.py
+++ async/management/commands/flush_queue.py
@@ -9,6 +9,29 @@ from lockfile import FileLock, AlreadyLocked
 from async.models import Job
 
 
+def acquire_lock(lockname):
+    """Return a decorator for the given lock name
+    """
+    def decorator(handler):
+        """Decorator for lock acquisition
+        """
+        def handle(self, **options):
+            """Acquire the lock before running the method.
+            """
+            lock = FileLock(lockname)
+            try:
+                lock.acquire(timeout = -1)
+            except AlreadyLocked:
+                print 'Lock is already set, aborting.'
+                return
+            try:
+                handler(self, **options)
+            finally:
+                lock.release()
+        return handle
+    return decorator
+
+
 class Command(BaseCommand):
     """
         Invoke using:
@@ -20,47 +43,37 @@ class Command(BaseCommand):
     )
     help = 'Does a single pass over the asynchronous queue'
 
+    @acquire_lock('async_flush_queue')
     def handle(self, **options):
         """Command implementation.
 
         This implementation is pretty ugly, but does behave in the
         right way.
         """
-
-        lock = FileLock('async_flush_queue')
-        try:
-            lock.acquire(timeout = -1)
-        except AlreadyLocked:
-            print 'Lock is already set, aborting.'
-            return
-
-        try:
-            while True:
-                now = datetime.now()
-                by_priority = (Job.objects
-                    .filter(executed=None)
-                    .exclude(scheduled__gt=now)
-                    .order_by('-priority'))
-                number = by_priority.count()
-                if number == 0:
-                    print "No jobs found for execution"
-                    return
-                def run(jobs):
-                    """Run the jobs handed to it
-                    """
-                    for job in jobs.iterator():
-                        print "%s: %s" % (job.id, unicode(job))
-                        job.execute()
-                        return False
-                    return True
-                priority = by_priority[0].priority
-                if run(Job.objects
-                        .filter(executed=None, scheduled__lte=now,
-                            priority=priority)
-                        .order_by('scheduled', 'id')):
-                    run(Job.objects
-                        .filter(executed=None, scheduled=None, priority=priority)
-                        .order_by('id'))
-        finally:
-            lock.release()
+        while True:
+            now = datetime.now()
+            by_priority = (Job.objects
+                .filter(executed=None)
+                .exclude(scheduled__gt=now)
+                .order_by('-priority'))
+            number = by_priority.count()
+            if number == 0:
+                print "No jobs found for execution"
+                return
+            def run(jobs):
+                """Run the jobs handed to it
+                """
+                for job in jobs.iterator():
+                    print "%s: %s" % (job.id, unicode(job))
+                    job.execute()
+                    return False
+                return True
+            priority = by_priority[0].priority
+            if run(Job.objects
+                    .filter(executed=None, scheduled__lte=now,
+                        priority=priority)
+                    .order_by('scheduled', 'id')):
+                run(Job.objects
+                    .filter(executed=None, scheduled=None, priority=priority)
+                    .order_by('id'))
 
