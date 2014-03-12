4d2e3efdf284ff59de0823a63c919d487728a18a
Kirit Sælensminde <k@kirit.com>
2014-03-12 14:13:49 +0700

    Refactor the job executor so we can parameterise it better

--- async/management/commands/flush_queue.py
+++ async/management/commands/flush_queue.py
@@ -15,21 +15,57 @@ def acquire_lock(lockname):
     def decorator(handler):
         """Decorator for lock acquisition
         """
-        def handle(self, **options):
+        def handle(*args):
             """Acquire the lock before running the method.
             """
             lock = FileLock(lockname)
             try:
                 lock.acquire(timeout=-1)
-            except AlreadyLocked:
+            except AlreadyLocked: # pragma: no cover
                 print 'Lock is already set, aborting.'
                 return
             try:
-                handler(self, **options)
+                handler(*args)
             finally:
                 lock.release()
         return handle
     return decorator
+
+
+def run_queue(_which, _outof, limit):
+    """
+        Code that actually executes the jobs in the queue.
+
+        This implementation is pretty ugly, but does behave in the
+        right way.
+    """
+    for _ in xrange(limit):
+        now = datetime.now()
+        by_priority = (Job.objects
+            .filter(executed=None)
+            .exclude(scheduled__gt=now)
+            .order_by('-priority'))
+        number = by_priority.count()
+        if number == 0:
+            print "No jobs found for execution"
+            return
+        def run(jobs):
+            """Run the jobs handed to it
+            """
+            for job in jobs.iterator():
+                print "%s: %s" % (job.id, unicode(job))
+                job.execute()
+                return False
+            return True
+        priority = by_priority[0].priority
+        if run(Job.objects
+                .filter(executed=None, scheduled__lte=now,
+                    priority=priority)
+                .order_by('scheduled', 'id')):
+            run(Job.objects
+                .filter(executed=None, scheduled=None,
+                    priority=priority)
+                .order_by('id'))
 
 
 class Command(BaseCommand):
@@ -43,39 +79,11 @@ class Command(BaseCommand):
     )
     help = 'Does a single pass over the asynchronous queue'
 
-    @acquire_lock('async_flush_queue')
     def handle(self, **options):
-        """Command implementation.
-
-        This implementation is pretty ugly, but does behave in the
-        right way.
+        """
+            Command implementation.
         """
         jobs_limit = int(options.get('jobs') or 300)
 
-        for _ in xrange(jobs_limit):
-            now = datetime.now()
-            by_priority = (Job.objects
-                .filter(executed=None)
-                .exclude(scheduled__gt=now)
-                .order_by('-priority'))
-            number = by_priority.count()
-            if number == 0:
-                print "No jobs found for execution"
-                return
-            def run(jobs):
-                """Run the jobs handed to it
-                """
-                for job in jobs.iterator():
-                    print "%s: %s" % (job.id, unicode(job))
-                    job.execute()
-                    return False
-                return True
-            priority = by_priority[0].priority
-            if run(Job.objects
-                    .filter(executed=None, scheduled__lte=now,
-                        priority=priority)
-                    .order_by('scheduled', 'id')):
-                run(Job.objects
-                    .filter(executed=None, scheduled=None, priority=priority)
-                    .order_by('id'))
+        acquire_lock('async_flush_queue')(run_queue)(0, 1, jobs_limit)
 
