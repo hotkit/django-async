1732db4ce2baa6e9031ea79a0c294a8ca1eedec3
Kirit Sælensminde <k@kirit.com>
2014-05-29 17:51:39 +0700

    Fix a priority inversion bug with the group final jobs.

--- async/management/commands/flush_queue.py
+++ async/management/commands/flush_queue.py
@@ -46,14 +46,6 @@ def run_queue(which, outof, limit):
     """
     for _ in xrange(limit):
         now = timezone.now()
-        by_priority = (Job.objects
-            .filter(executed=None, cancelled=None)
-            .exclude(scheduled__gt=now)
-            .order_by('-priority'))
-        number = by_priority.count()
-        if number == 0:
-            print "No jobs found for execution"
-            return
         def run(jobs):
             """Run the jobs handed to it
             """
@@ -67,15 +59,30 @@ def run_queue(which, outof, limit):
                     job.execute()
                     return False
             return True
-        priority = by_priority[0].priority
-        if run(Job.objects
-                .filter(executed=None, scheduled__lte=now,
-                    priority=priority)
-                .order_by('scheduled', 'id')):
-            run(Job.objects
-                .filter(executed=None, scheduled=None,
-                    priority=priority)
-                .order_by('id'))
+        by_priority = by_priority_filter = (Job.objects
+            .filter(executed=None, cancelled=None)
+            .exclude(scheduled__gt=now)
+            .order_by('-priority'))
+        while True:
+            number = by_priority.count()
+            if number == 0:
+                print "No jobs found for execution"
+                return
+            priority = by_priority[0].priority
+            if run(Job.objects
+                    .filter(executed=None, cancelled=None,
+                        scheduled__lte=now, priority=priority)
+                    .order_by('scheduled', 'id')):
+                if run(Job.objects
+                        .filter(executed=None, cancelled=None,
+                            scheduled=None, priority=priority)
+                        .order_by('id')):
+                    by_priority = by_priority_filter.filter(
+                        priority__lt=priority)
+                else:
+                    break
+            else:
+                break
 
 
 class Command(BaseCommand):
