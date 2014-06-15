4f3633e9f7a148f3ce9a185794a5856d99b68420
Kirit Sælensminde <k@kirit.com>
2014-06-15 11:30:06 +0700

    Use the group reference to generate the progress URL.

--- async/slumber_operations.py
+++ async/slumber_operations.py
@@ -5,6 +5,7 @@ from django.shortcuts import Http404
 from django.db.models import Count
 from slumber.operations import ModelOperation, InstanceOperation
 from slumber.server.http import require_user, require_permission
+from urllib import quote
 
 from async import schedule
 from async.models import Group
@@ -43,6 +44,14 @@ class Schedule(ModelOperation):
 class Progress(InstanceOperation):
     """Return information about the progress of a job.
     """
+    def __call__(self, group):
+        """Generate the operation URL using the reference instead
+            of the primary key.
+        """
+        # pylint: disable=arguments-differ
+        part = quote(group.reference.encode('utf-8'), '')
+        return super(Progress, self).__call__() + part + '/'
+
     @require_user
     def get(self, _request, response, _app, _models, group_reference_name):
         """The current progress and estimated completion time of the job.
