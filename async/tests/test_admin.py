2fa8c987a2a5ebf8b6cc373eeaa2888c550f58e0
jiitoey <chirapat@proteus-tech.com>
2014-07-08 14:43:42 +0700

    add tests for Job admin display_group

---
+++ async/tests/test_admin.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+import async
+from async.admin import display_group
+from async.models import Group
+
+
+class TestJobAdmin(TestCase):
+    def do_nothing(self):
+        return True
+
+    def test_display_group__have_group(self):
+        group = Group.latest_group_by_reference('nothing')
+        job = async.schedule('async.tests.test_admin.TestJobAdmin.do_nothing', group=group)
+
+        group_to_be_displayed = display_group(job)
+        self.assertEqual(group_to_be_displayed, group.reference)
+
+    def test_display_group__do_not_have_group(self):
+        job = async.schedule('async.tests.test_admin.TestJobAdmin.do_nothing')
+
+        group = display_group(job)
+        self.assertEqual(group, None)
