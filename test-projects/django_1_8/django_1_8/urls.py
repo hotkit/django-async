4d6ab3a4bc512d618bf2ce833a99be82d6d58d55
Kirit Sælensminde <k@kirit.com>
2015-04-08 16:36:59 +0700

    Add slumber to the URLs.

--- test-projects/django_1_8/django_1_8/urls.py
+++ test-projects/django_1_8/django_1_8/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     # url(r'^$', 'django_1_8.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # Enable Slumber
+    (r'^slumber/', include('slumber.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
 ]
