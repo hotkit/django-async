43cf78415a894efd3ddb85f371d1eb50360b52da
Kirit Sælensminde <k@kirit.com>
2015-04-08 16:25:12 +0700

    Use postgres as the database engine.

--- test-projects/django_1_8/django_1_8/settings.py
+++ test-projects/django_1_8/django_1_8/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_1_8.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'async-exec',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
