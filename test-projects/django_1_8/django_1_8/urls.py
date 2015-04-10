c851194e61be161261c930fb11dee553c42d2ca8
Kirit Sælensminde <k@kirit.com>
2015-04-10 09:56:14 +0700

    Fix up URLs for Django 1.8.

--- test-projects/django_1_8/django_1_8/urls.py
+++ test-projects/django_1_8/django_1_8/urls.py
@@ -2,12 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'django_1_8.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     # Enable Slumber
-    (r'^slumber/', include('slumber.urls')),
+    url(r'^slumber/', include('slumber.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
