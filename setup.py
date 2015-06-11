ac1d312368312f31e330861ba9c1a2be0d5bc69a
Wutcharin(Klangggg) <wutcharin.m@proteus-tech.com>
2015-06-11 14:41:24 +0700

    update ChangeLog

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info >= (3,):
 
 setup(
     name = "django-async",
-    version = "0.6.0.12",
+    version = "0.6.0.13",
     author = "Kirit Saelensminde",
     author_email = "kirit@felspar.com",
     url='http://www.kirit.com/Django%20Async',
