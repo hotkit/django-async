7143c0a29e6163ed6ef5cef9bc2909981b7886f7
Tobias Carlander <tobias.carlander@wfp.org>
2017-02-15 14:11:30 +0700

    Adding 1.9 - 1.11

--- test-projects/django_1_11/django_1_11/urls.py
+++ test-projects/django_1_11/django_1_11/urls.py
@@ -1,7 +1,7 @@
-"""django_1_9 URL Configuration
+"""django_1_11 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^slumber/', include('slumber.urls')),
-
-
     url(r'^admin/', admin.site.urls),
 ]
