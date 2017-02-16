476d350a4dd1a5c1a4c6f5ffb00a1a59042abf03
Tobias Carlander <tobias.carlander@wfp.org>
2017-02-16 10:48:10 +0700

    Fix test faults

--- async/utils.py
+++ async/utils.py
@@ -12,13 +12,11 @@ def full_name(item):
         unicode = unicode
     except NameError:
         # 'unicode' is undefined, must be Python 3
-        str = str
         unicode = str
         bytes = bytes
         basestring = (str, bytes)
     else:
         # 'unicode' exists, must be Python 2
-        str = str
         unicode = unicode
         bytes = str
         basestring = basestring
