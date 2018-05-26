18eeb8fed051394ecc522acb16e6a86c6817b69f
Kirit Sælensminde <k@kirit.com>
2018-05-26 12:52:36 +0700

    Use the default postgres user.

--- test-projects/django_1_11/django_1_11/settings.py
+++ test-projects/django_1_11/django_1_11/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'async-exec',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
+        'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
